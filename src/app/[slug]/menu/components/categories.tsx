ced8f47707e62bedccc68121c5a766762704b3b0
Yaba Ernesto <yabaernesto@gmail.com>
2025-02-21 23:58:28 +0100

    feat: Category menu

---
+++ src/app/[slug]/menu/components/categories.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { type MenuCategory, Prisma } from "@prisma/client";
+import { ClockIcon } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+
+interface RestaurantCategoriesProps {
+  restaurant: Prisma.RestaurantGetPayload<{
+    include: {
+      menuCategories: {
+        include: {
+          products: true;
+        };
+      };
+    };
+  }>;
+}
+
+const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
+  const [selectCategory, setSelectCategory] = useState<MenuCategory>(
+    restaurant.menuCategories[0],
+  );
+
+  const handleCategoryClick = (category) => {};
+
+  return (
+    <div className="relative z-50 mt-[-1.5rem] rounded-t-2xl border bg-white">
+      <div className="p-5">
+        <div className="flex items-center gap-3">
+          <Image
+            src={restaurant.avatarImageUrl}
+            alt={restaurant.name}
+            height={45}
+            width={45}
+          />
+          <div>
+            <h2 className="text-lg font-semibold">{restaurant.name}</h2>
+            <p className="text-xs opacity-55">{restaurant.description}</p>
+          </div>
+        </div>
+        <div className="mt-3 flex items-center gap-1 text-xs text-green-500">
+          <ClockIcon size={12} />
+          <p>Aberto</p>
+        </div>
+      </div>
+
+      <ScrollArea className="w-full">
+        <div className="flex w-max space-x-4 p-5 pt-0">
+          {restaurant.menuCategories.map((category) => (
+            <Button
+              key={category.id}
+              variant="secondary"
+              size="sm"
+              className="rounded-full"
+              onClick={() => handleCategoryClick(category)}
+            >
+              {category.name}
+            </Button>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+    </div>
+  );
+};
+
+export default RestaurantCategories;
--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
+import RestaurantCategories from "./components/categories";
 import RestaurantHeader from "./components/header";
 
 interface RestaurantMenuPageProps {
@@ -24,7 +25,16 @@ const RestaurantMenuPage = async ({
     return notFound();
   }
 
-  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  const restaurant = await db.restaurant.findUnique({
+    where: { slug },
+    include: {
+      menuCategories: {
+        include: {
+          products: true,
+        },
+      },
+    },
+  });
 
   if (!restaurant) {
     return notFound();
@@ -33,6 +43,7 @@ const RestaurantMenuPage = async ({
   return (
     <div>
       <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
     </div>
   );
 };
