f8689dcdc9988feb6ee236f7027db1e7d47d0083
Yaba Ernesto <yabaernesto@gmail.com>
2025-02-23 22:41:55 +0100

    feat: add product header component

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,16 +1,23 @@
 import { Product } from "@prisma/client";
 import Image from 'next/image'
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 interface ProductsProps {
-  products: Product[]
+  products: Product[];
 }
 
 const Products = ({ products }: ProductsProps) => {
+  const { slug } = useParams<{ slug: string }>();
+
   return (
     <div className="space-y-3 px-5"> 
       {products.map(product => (
-        <Link key={product.id} href="/" className="flex items-center justify-between gap-10 border-b py-3">
+        <Link 
+          key={product.id} 
+          href={`/${slug}/menu/${product.id}`}
+          className="flex items-center justify-between gap-10 border-b py-3"
+        >
           {/* ESQUERDA */}
           <div>
             <h3 className="text-sm font-medium">{product.name}</h3>
