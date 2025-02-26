dd94bf463b29edded016810769e6cc1d0d31f176
Yaba Ernesto <yabaernesto@gmail.com>
2025-02-26 02:06:52 +0100

    feat: add orders page

--- src/app/[slug]/menu/actions/create-order.tsx
+++ src/app/[slug]/menu/actions/create-order.tsx
@@ -1,6 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
+import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
@@ -60,4 +61,6 @@ export const createOrder = async (input: createOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
+  // redirecionar
+  redirect(`/${input.slug}/orders`);
 };
---
+++ src/app/[slug]/orders/components/order-list.tsx
@@ -0,0 +1,93 @@
+import { OrderStatus, Prisma } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { formatCurrency } from "@/helpers/format-currency";
+
+interface OrderListProps {
+  orders: Array<
+    Prisma.OrderGetPayload<{
+      include: {
+        restaurant: {
+          select: {
+            name: true,
+            avatarImageUrl: true,
+          },
+        },
+        orderProducts: {
+          include: {
+            product: true,
+          },
+        },
+      },
+    }>
+  >;
+};
+
+const getStatusLabel = (status: OrderStatus) => {
+  if (status === "FINISHED") return "Finalizado"
+  if (status === "IN_PREPARATION") return "Em preparo"
+  if (status === "PENDING") return "Pendente"
+  return ""
+};
+
+const OrderList = ({ orders }: OrderListProps) => {
+  return (
+    <div className="space-y-6 p-6">
+      <Button size="icon" variant="secondary" className="rounded-full">
+        <ChevronLeftIcon />
+      </Button>
+
+      <div className="flex items-center gap-3">
+        <ScrollTextIcon />
+        <h2 className="text-lg font-semibold">Meus Pedidos</h2>
+      </div>
+
+      {orders.map(order => (
+        <Card key={order.id}>
+          <CardContent className="space-y-4 p-5">
+            <div className={`w-fit rounded-full text-white px-2 py-1 text-xs font-semibold
+              ${
+                order.status === OrderStatus.FINISHED ? "bg-green-500 text-white"
+                : "bg-gray-200 text-gray-500"
+              }  
+            `}>
+              {getStatusLabel(order.status)}
+            </div>
+            <div className="flex items-center gap-2">
+              <div className="relative h-5 w-5">
+                <Image
+                  src={order.restaurant.avatarImageUrl}
+                  alt={order.restaurant.name}
+                  className="rounded-sm"
+                  fill
+                />
+              </div>
+              <p className="font-semibold text-sm">{order.restaurant.name}</p>
+            </div>
+            <Separator />
+            <div className="space-y-2">
+              {order.orderProducts.map(orderProduct => (
+                <div key={orderProduct.id} className="flex items-center gap-2">
+                  <div className="h-4 w-4 flex items-center justify-center rounded-full gb-gray-400 text-white text-xs font-semibold">
+                    {orderProduct.quantity}
+                  </div>
+                  <p className="text-sm ">{orderProduct.product.name}</p>
+                </div>
+              ))}
+            </div>
+            <Separator />
+            <p className="text-sm font-medium">
+              {formatCurrency(order.total)}
+            </p>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+}
+ 
+export default OrderList;
